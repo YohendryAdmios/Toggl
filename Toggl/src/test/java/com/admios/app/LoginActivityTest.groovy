e3103cde00e4ad25158d55fc855e46bd6610f2b4
Yohendry Hurtado <yohendry.hurtado@rightscale.com>
2014-02-28 23:47:16 -0500

    adding test test

---
+++ Toggl/src/test/java/com/admios/app/LoginActivityTest.groovy
@@ -0,0 +1,12 @@
+package test.java.com.admios.app
+
+/**
+ * Created by yohendryhurtado on 2/28/14.
+ */
+class LoginActivityTest extends groovy.util.GroovyTestCase {
+
+    void testSomething() {
+        assert 1 == 1
+        assert 2 + 2 == 4 : "We're in trouble, arithmetic is broken"
+    }
+}
