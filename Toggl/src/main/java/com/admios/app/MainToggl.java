173f2b50b60c0cbe9314508e7203c5dc97b9a184
Yohendry Hurtado <yohendry.hurtado@rightscale.com>
2014-03-05 12:10:26 -0500

    create actionbar menu, load the default profile image for the current user and display de profile submenu

--- Toggl/src/main/java/com/admios/app/MainToggl.java
+++ Toggl/src/main/java/com/admios/app/MainToggl.java
@@ -1,11 +1,16 @@
 package com.admios.app;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,7 +18,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.admios.model.User;
+import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 public class MainToggl extends ActionBarActivity implements ActionBar.OnNavigationListener {
 
@@ -22,53 +34,76 @@ public class MainToggl extends ActionBarActivity implements ActionBar.OnNavigati
      * current dropdown position.
      */
     private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+  private User user;
+  private MenuItem mi;
 
-    @Override
+  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_toggl);
 
-        // Set up the action bar to show a dropdown list.
-        final ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+      user = getUser();
+      this.setTitle(user.getData().getFullname());
 
-        // Set up the dropdown list navigation in the action bar.
-        actionBar.setListNavigationCallbacks(
-                // Specify a SpinnerAdapter to populate the dropdown list.
-                new ArrayAdapter<String>(
-                        actionBar.getThemedContext(),
-                        android.R.layout.simple_list_item_1,
-                        android.R.id.text1,
-                        new String[] {
-                                getString(R.string.title_section1),
-                                getString(R.string.title_section2),
-                                getString(R.string.title_section3),
-                        }),
-                this);
 
-      TextView tv = (TextView)findViewById(R.id.textView);
-      tv.setText(getIntent().getStringExtra("user"));
+
     }
+
+    private User getUser(){
+      Gson gson = new Gson();
+      return gson.fromJson(getIntent().getStringExtra("user"), User.class);
+    }
+
 
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_toggl, menu);
+      getMenuInflater().inflate(R.menu.main_toggl, menu);
+
+      mi = (MenuItem)menu.findItem(R.id.action_profile);
+      LoadImageTask lit = new LoadImageTask();
+      lit.execute((Void)null);
+      return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(int position, long id) {
         return true;
     }
 
+  private class LoadImageTask extends AsyncTask<Void, Void, Drawable> {
+
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return false;
+    protected Drawable doInBackground(Void... voids) {
+      try {
+        Bitmap bm = Picasso.with(getApplicationContext())
+                .load(user.getData().getImageUrl())
+                .placeholder(R.drawable.ic_action_search)
+                .error(R.drawable.abc_ic_clear)
+                .get();
+        Drawable d = new BitmapDrawable(getResources(), bm);
+        return d;
+      } catch (IOException e) {
+        Log.e("HEY",e.getMessage());
+        e.printStackTrace();
+      }
+      return null;
     }
 
+    @Override
+    protected void onPostExecute(Drawable drawable) {
+      if(drawable != null){
+        changeProfileData(drawable);
+      }
+    }
+  }
 
-  @Override
-  public boolean onNavigationItemSelected(int i, long l) {
-    return false;
+
+  private void changeProfileData(Drawable drawable) {
+    mi.setIcon(drawable);
+    mi.setTitle(user.getData().getFullname());
   }
 }
